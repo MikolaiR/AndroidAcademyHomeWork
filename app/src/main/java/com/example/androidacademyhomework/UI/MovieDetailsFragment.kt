e0a5bd74b0c2098540f4cd0fa8ac985c9f49c5ac
Mikalai <mikalairnn@gmail.com>
2020-11-22 18:36:16 +0300

    fix: rotation for imageAfter

--- app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.androidacademyhomework.R
 import com.example.androidacademyhomework.databinding.FragmentMovieDetailsBinding
 
 class MovieDetailsFragment : Fragment() {
 
-    private lateinit var movieDetailsBinding: FragmentMovieDetailsBinding
+    private lateinit var fragmentMovieDetailsBinding: FragmentMovieDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        fragmentMovieDetailsBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_movie_details, container, false)
+        return fragmentMovieDetailsBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
