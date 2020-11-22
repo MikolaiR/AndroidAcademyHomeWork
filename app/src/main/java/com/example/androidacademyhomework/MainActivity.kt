eba19a599507490a0d946319aa6fffc3a65841a6
Mikalai <mikalairnn@gmail.com>
2020-11-22 17:18:19 +0300

    feat: add navigation fragment

--- app/src/main/java/com/example/androidacademyhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidacademyhomework/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.example.androidacademyhomework
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
-import com.example.androidacademyhomework.adapter.AdapterRecyclerViewMoveDetail
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.button_next_activity).setOnClickListener {
-            startActivity(Intent(this,MovieDetailsActivity::class.java))
-        }
     }
 }
---
+++ app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.androidacademyhomework.UI
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidacademyhomework.R
+import com.example.androidacademyhomework.databinding.FragmentMovieDetailsBinding
+
+class MovieDetailsFragment : Fragment() {
+
+    private lateinit var movieDetailsBinding: FragmentMovieDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+}
