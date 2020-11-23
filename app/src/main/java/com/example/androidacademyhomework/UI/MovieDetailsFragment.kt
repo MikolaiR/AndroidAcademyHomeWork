58f4598cb2b1fd40cf6a111ba4ee863c046f0194
Mikalai <mikalairnn@gmail.com>
2020-11-23 14:51:54 +0300

    fix: fixing mistakes in homework

--- app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
@@ -5,24 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.example.androidacademyhomework.R
-import com.example.androidacademyhomework.databinding.FragmentMovieDetailsBinding
 
 class MovieDetailsFragment : Fragment() {
-
-    private lateinit var fragmentMovieDetailsBinding: FragmentMovieDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentMovieDetailsBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_movie_details, container, false)
-        return fragmentMovieDetailsBinding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
     }
 }
