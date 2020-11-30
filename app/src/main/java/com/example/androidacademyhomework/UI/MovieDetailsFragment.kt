9c1bfe3913fc738479ff7950eb54bfcffa46a715
Mikalai <mikalairnn@gmail.com>
2020-11-30 03:18:43 +0300

    feat: added navigation of transitions between fragments

--- app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/androidacademyhomework/UI/MovieDetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.androidacademyhomework.R
 
 class MovieDetailsFragment : Fragment() {
@@ -13,7 +15,18 @@ class MovieDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.buttonViewBack).setOnClickListener {
+            returnToMovieListFragment()
+        }
+    }
+
+    private fun returnToMovieListFragment() {
+       findNavController().navigate( R.id.action_movieDetailsFragment_to_movieListFragment)
     }
 }
--- app/src/main/java/com/example/androidacademyhomework/UI/MovieListFragment.kt
+++ app/src/main/java/com/example/androidacademyhomework/UI/MovieListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
 import com.example.androidacademyhomework.R
 
 class MovieListFragment : Fragment() {
@@ -13,7 +15,18 @@ class MovieListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ConstraintLayout>(R.id.constraintLayoutMovieOne).setOnClickListener {
+            goToMovieDetailFragment()
+        }
+    }
+
+    private fun goToMovieDetailFragment(){
+        findNavController().navigate(R.id.action_movieListFragment_to_movieDetailsFragment)
     }
 }
