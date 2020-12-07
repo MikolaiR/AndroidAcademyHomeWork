a69267a6459420d04e750aebf48336915f2547cd
Mikalai <mikalairnn@gmail.com>
2020-12-07 16:19:28 +0300

    feat: added a recycler implementation and a transition method with the transfer of the id of the selected movie

--- app/src/main/java/com/example/androidacademyhomework/UI/MovieListFragment.kt
+++ app/src/main/java/com/example/androidacademyhomework/UI/MovieListFragment.kt
@@ -7,9 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidacademyhomework.R
+import com.example.androidacademyhomework.adapters.MovieAdapter
+import com.example.androidacademyhomework.adapters.OnRecyclerItemClicked
+import com.example.androidacademyhomework.domain.MoviesDataSource
+import com.example.androidacademyhomework.models.Movie
 
 class MovieListFragment : Fragment() {
+
+    private var recyclerMovieList:RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,13 +29,43 @@ class MovieListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recyclerMovieList = view.findViewById(R.id.recyclerMovieList)
+        initRecycler()
+    }
 
-        view.findViewById<ConstraintLayout>(R.id.constraintLayoutMovieOne).setOnClickListener {
-            goToMovieDetailFragment()
+    override fun onStart() {
+        super.onStart()
+        updateData()
+    }
+
+    private fun initRecycler() {
+        recyclerMovieList?.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = MovieAdapter(clickListener)
+            itemAnimator = DefaultItemAnimator()
+            hasFixedSize()
         }
     }
 
-    private fun goToMovieDetailFragment(){
-        findNavController().navigate(R.id.action_movieListFragment_to_movieDetailsFragment)
+    private val clickListener = object : OnRecyclerItemClicked {
+        override fun onClick(movie:Movie) {
+            goToMovieDetailFragment(movie.id)
+        }
+    }
+
+    private fun updateData() {
+        (recyclerMovieList?.adapter as? MovieAdapter)?.apply {
+            bindActors(MoviesDataSource().getMovies())
+        }
+    }
+
+    override fun onDetach() {
+        recyclerMovieList = null
+        super.onDetach()
+    }
+
+    private fun goToMovieDetailFragment(movieId:Int){
+        val direction = MovieListFragmentDirections.actionMovieListFragmentToMovieDetailsFragment(movieId)
+        findNavController().navigate(direction)
     }
 }
