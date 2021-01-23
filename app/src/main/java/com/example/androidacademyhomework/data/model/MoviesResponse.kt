ca972537b72d64c6b2862551748fdf597ba90394
Mikalai <mikalairnn@gmail.com>
2021-01-23 13:06:03 +0300

    fix: remove unused fields and classes

--- app/src/main/java/com/example/androidacademyhomework/data/model/MoviesResponse.kt
+++ app/src/main/java/com/example/androidacademyhomework/data/model/MoviesResponse.kt
@@ -1,31 +1,15 @@
 package com.example.androidacademyhomework.data.model
 
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
 data class MoviesApiResponse(
 	@Json(name = "page")
 	val page: Int,
-	val totalPages: Int,
 	@Json(name = "results")
-	val results: List<MovieResult>,
-	val totalResults: Int
+	val results: List<MovieResult>
 )
 
 data class MovieResult(
 	@field:Json(name="id")
-	val id: Int,
-	val overview: String,
-	val originalLanguage: String,
-	val originalTitle: String,
-	val video: Boolean,
-	val title: String,
-	val genreIds: List<Int>,
-	val posterPath: String,
-	val backdropPath: String,
-	val releaseDate: String,
-	val popularity: Double,
-	val voteAverage: Double,
-	val adult: Boolean,
-	val voteCount: Int
+	val id: Int
 )
